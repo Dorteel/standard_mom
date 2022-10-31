6f53ff0728dda02cb57469267f0e26c189bc3bca
Mark Adamik <m.adamik@vu.nl>
2022-10-31 17:10:54 +0100

    ADD: New test folder, with my  tests of integrating SOSA into the Knowledgegraphs

---
+++ test/test_sosa.py
@@ -0,0 +1,17 @@
+from rdflib import Graph, Literal, RDF, URIRef
+from rdflib.namespace import SOSA
+
+# Create an empty Graph
+sensorGraph = Graph()
+
+locobot = URIRef("http://example.org/locobot")
+locobotCamera = URIRef("http://example.org/camera")
+
+# Add observation
+sensorGraph.add((locobotCamera, RDF.type, SOSA.Actuator))
+sensorGraph.add((locobot, SOSA.madeByActuator, locobotCamera))
+
+# Print out the entire Graph in the RDF Turtle format
+#print(sosaGraph.serialize(format="turtle"))
+
+print(sensorGraph.serialize(format="turtle"))
