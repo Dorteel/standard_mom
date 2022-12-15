237699312e1c9d3f6329b641aa258fa9d7ca7aee
Mark Adamik <m.adamik@vu.nl>
2022-12-15 16:38:32 +0100

    ADD: combining the vision components

--- scripts/perception/test_objectmatching.py
+++ scripts/perception/test_objectmatching.py
@@ -5,6 +5,27 @@ import cv2
 import numpy as np
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
 from interbotix_perception_modules.yolo import InterbotixYoloInterface
+from rdflib import Graph, Literal, RDF, URIRef, Namespace
+from rdflib.namespace import SOSA
+
+class DetectedObject():
+    def __init__(self, pos=None, name=None, color=None, npoints=None, prob=None, bbox=None, label=None):
+        self.name = name
+        self.position = pos
+        self.numberOfPoints = npoints
+        self.label=label
+        self.prob = prob
+        self.bbox = bbox
+        self.color = color
+
+    def sameAs(self, object):
+        '''
+        Checks whether the object is the same within some boundary
+        '''
+        pass
+
+
+
 
 class VisionNode(object):
     def __init__(self):
@@ -15,8 +36,8 @@ class VisionNode(object):
         self.clusters = []
         self.detections = []
         self.br = CvBridge()
-        # Node cycle rate (in Hz).
-        self.loop_rate = rospy.Rate(1)
+        self.objects = []
+        self.loop_rate = rospy.Rate(100)
 
         # Publishers
         self.pub = rospy.Publisher('test_image', Image,queue_size=20)
@@ -51,11 +72,40 @@ class VisionNode(object):
             '''
             Looks through the detected clusters and matches them with the detected bounding boxes
             '''
-            for cluster in self.clusters:
-                    px, py = self.get_pixel_from3d(cluster['position'])
-                    for d in self.detections:
-                        if px in range(d.xmin, d.xmax) and py in range(d.ymin, d.ymax):
-                            print('Cluster {} is a {}'.format(cluster['name'], d.Class))
+            objects = []
+            for c in self.clusters:
+                objects.append(DetectedObject(pos=c['position'],
+                name=c['name'], npoints=c['num_points'], color=c['color']))
+                px, py = self.get_pixel_from3d(c['position'])
+                for d in self.detections:
+                    if px in range(d.xmin, d.xmax) and py in range(d.ymin, d.ymax):
+                        objects[-1].name = d.Class
+                        objects[-1].prob = d.probability
+                        objects[-1].bbox = [d.xmin, d.ymin, d.xmax, d.ymax]
+                        print('Cluster {} is a {}'.format(c['name'], d.Class))
+            return objects
+
+    def generateSceneGraph(self, objects):
+        """
+        Scene graphs are generated from the detection and returned in a graph
+        
+        """
+        sceneGraph = Graph()
+
+        for cluser in self.clusters:
+            observation = URIRef("http://example.org/Observation/" + cluser['name'])
+            sceneGraph.add((observation, RDF.type, SOSA.Observation))
+            sceneGraph.add((observation, self.locobot.hasPosition, Literal(str(cluser['position']))))
+            sceneGraph.add((observation, self.locobot.hasColor, Literal(str(cluser['color']))))
+            sceneGraph.add((observation, self.locobot.hasNumberOfPoints, Literal(str(cluser['num_points']))))
+        for detection in self.detections:
+            bbox = [detection.xmin, detection.ymin, detection.xmax, detection.ymax]
+            observation = URIRef("http://example.org/Observation/" + detection.Class)
+            sceneGraph.add((observation, RDF.type, SOSA.Observation))
+            sceneGraph.add((observation, self.locobot.hasLabel, Literal(detection.Class)))
+            sceneGraph.add((observation, self.locobot.hasProbability, Literal(detection.probability)))
+            sceneGraph.add((observation, self.locobot.hasBoundingBox, Literal(bbox)))
+        return sceneGraph
 
 
     def start(self):
@@ -70,7 +120,7 @@ class VisionNode(object):
             self.detections = self.yolo.getDetected()
             print(self.detections)
             self.show_clusters(self.bot, 'locobot/camera_color_optical_frame')
-            self.get_objects()
+            self.objects = self.get_objects()
             if z >= z_uplimit or z <= z_lowlimit: d*=-1
             z+= d
             if self.image is not None:
